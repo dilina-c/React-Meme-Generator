d5ab12ff97f8619664cf5c759a8446b9ef38c899
dilina-c <2019s17383@stu.cmb.ac.lk>
2023-06-02 03:10:37 +0530

    meme generate without states

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,14 +1,26 @@
 import React from "react";
+import memeData from "../memeData";
 
 export default function Meme() {
+    // const [meme,setmemeImage]=React.useState("http://i.imgflip.com/1bij.jpg");
+    const [meme,setMeme]=React.useState({
+        topText:"",
+        bottomText:"",
+        randomImg:"http://i.imgflip.com/1bij.jpg"
+    });
+    function handleclick(){
+        const memearray=memeData.data.memes;
+        const randNum = Math.floor((Math.random() * memearray.length));
+        setmemeImage(url => memearray[randNum].url)
+    }
     return (
        <div className="meme">
-                <form className="meme--form">
+                <div className="meme--form">
                     <input type="text" name="topText" placeholder="Top Text" className="meme--form--input" />
                     <input type="text" name="bottomText" placeholder="Bottom Text" className="meme--form--input" />
-                    <button className="meme--button">Get a new meme image  🖼</button>
-                </form>
- 
+                    <button onClick={handleclick} className="meme--button">Get a new meme image  🖼</button>
+                </div>
+            {meme !==undefined && <img src={meme} alt="meme" className="meme--image" />}
        </div>
     )
 }
